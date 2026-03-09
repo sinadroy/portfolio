d5c4f26461f19a01cfb41296810f9a65d14d956a
yordanis-arencibia <yordanis.arencibia@halopowered.com>
2026-03-09 18:32:49 -0400

    feat: add documentation page with its route and navigation link.

---
+++ src/pages/Documentation.tsx
@@ -0,0 +1,8 @@
+export function Documentation() {
+  return (
+    <section>
+      <h1>Documentation</h1>
+      <p>Here you can find the documentation for the project.</p>
+    </section>
+  );
+}
